8a30b5ea44db17a44dd9b6800521f71801072cbd
Becky Gilbert <beckyannegilbert@gmail.com>
2017-03-24 14:06:59 +0000

    intial commit, store trial data in js/json

---
+++ shifttest_trial_data.js
@@ -0,0 +1,454 @@
+// BZ shift test trial data for 2 versions
+
+// all_trial_data is an array of JSON-like objects. Each object has the version number and trial data. The trial data is an array of objects, where each of the objects contains the data for a single trial. 
+var all_trial_data = [
+{ // test 1 version
+	version: "1",
+	trial_data: [
+  {
+    "trialNumber": 1,
+    "condition": 6,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_two_gold_fly_left.wav",
+    "fileName": "6_0_1_2_final_two_gold_fly_left.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 2,
+    "condition": 6,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 2,
+    "origFileName": "final_four_gold_cat_down.wav",
+    "fileName": "6_0_3_2_final_four_gold_cat_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 3,
+    "condition": 2,
+    "shift": 1,
+    "syllableNumber": 3,
+    "shiftDirection": 2,
+    "origFileName": "final_three_black_cow_left.wav",
+    "fileName": "2_1_3_2_final_three_black_cow_left.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 4,
+    "condition": 4,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 2,
+    "origFileName": "final_three_black_bee_left.wav",
+    "fileName": "4_0_3_2_final_three_black_bee_left.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 5,
+    "condition": 4,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_nine_black_cat_right.wav",
+    "fileName": "4_0_3_1_final_nine_black_cat_right.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 6,
+    "condition": 5,
+    "shift": 1,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_one_grey_cat_down.wav",
+    "fileName": "5_1_3_1_final_one_grey_cat_down.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 7,
+    "condition": 5,
+    "shift": 1,
+    "syllableNumber": 2,
+    "shiftDirection": 1,
+    "origFileName": "final_eight_grey_pig_left.wav",
+    "fileName": "5_1_2_1_final_eight_grey_pig_left.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 8,
+    "condition": 6,
+    "shift": 0,
+    "syllableNumber": 2,
+    "shiftDirection": 2,
+    "origFileName": "final_eight_green_dog_right.wav",
+    "fileName": "6_0_2_2_final_eight_green_dog_right.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 9,
+    "condition": 2,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_one_blue_sheep_down.wav",
+    "fileName": "2_0_1_1_final_one_blue_sheep_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 10,
+    "condition": 5,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 2,
+    "origFileName": "final_three_blue_mouse_down.wav",
+    "fileName": "5_0_3_2_final_three_blue_mouse_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 11,
+    "condition": 5,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_two_green_frog_right.wav",
+    "fileName": "5_1_1_1_final_two_green_frog_right.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 12,
+    "condition": 2,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_five_black_cat_right.wav",
+    "fileName": "2_0_1_2_final_five_black_cat_right.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 13,
+    "condition": 1,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_one_grey_cow_down.wav",
+    "fileName": "1_0_1_1_final_one_grey_cow_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 14,
+    "condition": 4,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_nine_red_ant_down.wav",
+    "fileName": "4_1_1_1_final_nine_red_ant_down.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 15,
+    "condition": 7,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_eight_black_mouse_down.wav",
+    "fileName": "7_0_3_1_final_eight_black_mouse_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 16,
+    "condition": 3,
+    "shift": 1,
+    "syllableNumber": 3,
+    "shiftDirection": 2,
+    "origFileName": "final_four_pink_fish_up.wav",
+    "fileName": "3_1_3_2_final_four_pink_fish_up.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 17,
+    "condition": 5,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_five_gold_cow_left.wav",
+    "fileName": "5_1_1_1_final_five_gold_cow_left.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 18,
+    "condition": 2,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_three_gold_horse_right.wav",
+    "fileName": "2_0_3_1_final_three_gold_horse_right.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 19,
+    "condition": 6,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_four_green_pig_right.wav",
+    "fileName": "6_0_1_2_final_four_green_pig_right.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 20,
+    "condition": 2,
+    "shift": 0,
+    "syllableNumber": 2,
+    "shiftDirection": 2,
+    "origFileName": "final_one_blue_mouse_down.wav",
+    "fileName": "2_0_2_2_final_one_blue_mouse_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 21,
+    "condition": 4,
+    "shift": 0,
+    "syllableNumber": 2,
+    "shiftDirection": 2,
+    "origFileName": "final_five_gold_fly_right.wav",
+    "fileName": "4_0_2_2_final_five_gold_fly_right.wav",
+    "correctResponse": "f"
+  }
+]
+},
+{ // test 1 version
+	version: "1",
+	trial_data: [
+  {
+    "trialNumber": 1,
+    "condition": 4,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_one_pink_horse_left.wav",
+    "fileName": "4_1_1_1_final_one_pink_horse_left.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 2,
+    "condition": 2,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_five_black_dog_up.wav",
+    "fileName": "2_0_1_2_final_five_black_dog_up.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 3,
+    "condition": 3,
+    "shift": 0,
+    "syllableNumber": 2,
+    "shiftDirection": 1,
+    "origFileName": "final_four_black_horse_down.wav",
+    "fileName": "3_0_2_1_final_four_black_horse_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 4,
+    "condition": 5,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_four_red_frog_up.wav",
+    "fileName": "5_1_1_2_final_four_red_frog_up.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 5,
+    "condition": 1,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_one_red_bee_left.wav",
+    "fileName": "1_0_1_2_final_one_red_bee_left.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 6,
+    "condition": 6,
+    "shift": 1,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_six_pink_ant_down.wav",
+    "fileName": "6_1_3_1_final_six_pink_ant_down.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 7,
+    "condition": 6,
+    "shift": 1,
+    "syllableNumber": 2,
+    "shiftDirection": 2,
+    "origFileName": "final_three_grey_frog_down.wav",
+    "fileName": "6_1_2_2_final_three_grey_frog_down.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 8,
+    "condition": 7,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_six_grey_cat_up.wav",
+    "fileName": "7_1_1_2_final_six_grey_cat_up.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 9,
+    "condition": 2,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_five_white_mouse_up.wav",
+    "fileName": "2_1_1_2_final_five_white_mouse_up.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 10,
+    "condition": 6,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_four_grey_sheep_up.wav",
+    "fileName": "6_0_1_1_final_four_grey_sheep_up.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 11,
+    "condition": 3,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_two_white_frog_right.wav",
+    "fileName": "3_0_1_2_final_two_white_frog_right.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 12,
+    "condition": 5,
+    "shift": 1,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_one_red_bird_left.wav",
+    "fileName": "5_1_3_1_final_one_red_bird_left.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 13,
+    "condition": 5,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_nine_black_cat_left.wav",
+    "fileName": "5_0_1_2_final_nine_black_cat_left.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 14,
+    "condition": 1,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 1,
+    "origFileName": "final_three_red_ant_up.wav",
+    "fileName": "1_0_3_1_final_three_red_ant_up.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 15,
+    "condition": 2,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_one_black_fly_up.wav",
+    "fileName": "2_1_1_2_final_one_black_fly_up.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 16,
+    "condition": 4,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_two_pink_mouse_up.wav",
+    "fileName": "4_0_1_2_final_two_pink_mouse_up.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 17,
+    "condition": 4,
+    "shift": 0,
+    "syllableNumber": 2,
+    "shiftDirection": 1,
+    "origFileName": "final_five_blue_ant_left.wav",
+    "fileName": "4_0_2_1_final_five_blue_ant_left.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 18,
+    "condition": 2,
+    "shift": 0,
+    "syllableNumber": 1,
+    "shiftDirection": 2,
+    "origFileName": "final_one_green_fly_left.wav",
+    "fileName": "2_0_1_2_final_one_green_fly_left.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 19,
+    "condition": 3,
+    "shift": 1,
+    "syllableNumber": 2,
+    "shiftDirection": 2,
+    "origFileName": "final_two_green_pig_right.wav",
+    "fileName": "3_1_2_2_final_two_green_pig_right.wav",
+    "correctResponse": "j"
+  },
+  {
+    "trialNumber": 20,
+    "condition": 5,
+    "shift": 0,
+    "syllableNumber": 3,
+    "shiftDirection": 2,
+    "origFileName": "final_one_black_ant_down.wav",
+    "fileName": "5_0_3_2_final_one_black_ant_down.wav",
+    "correctResponse": "f"
+  },
+  {
+    "trialNumber": 21,
+    "condition": 2,
+    "shift": 1,
+    "syllableNumber": 1,
+    "shiftDirection": 1,
+    "origFileName": "final_three_black_bat_right.wav",
+    "fileName": "2_1_1_1_final_three_black_bat_right.wav",
+    "correctResponse": "j"
+  }
+]
+}
+
+
+
+// { // original structure
+// 	version: "2",
+// 	trial_data:[
+// 	{
+// 		stimulus: "/study_assets/BZ_Shift_Test_Demo_Feedback/test2/2_0_1_2_final_one_green_fly_left.mp3", 
+// 		data: {condition: 2, shift: 0, syllable: 1, direction: 2, correctResponse: 'f'}
+// 	},
+// 	{
+// 		stimulus: "/study_assets/BZ_Shift_Test_Demo_Feedback/test2/3_0_1_2_final_two_white_frog_right.mp3", 
+// 		data: {condition: 3, shift: 0, syllable: 1, direction: 2, correctResponse: 'f'}},
+// 	{
+// 		stimulus: "/study_assets/BZ_Shift_Test_Demo_Feedback/test2/4_1_1_1_final_one_pink_horse_left.mp3", 
+// 		data: {condition: 4, shift: 1, syllable: 1, direction: 1, correctResponse: 'j'}
+// 	}
+// 	]
+// }
+// ];
